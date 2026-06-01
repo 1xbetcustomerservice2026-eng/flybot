6ab6b3133364029d809ca656044d5f009bbc4e22
1xbetcustomerservice2026-eng <1xbetcustomerservice2026@gmail.com>
2026-06-01 23:13:09 +0300

    config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 _BASE_DIR = Path(__file__).parent.parent
 
-BOT_TOKEN: str = os.environ.get("8869833081:AAHS9V7cGFwjjQE5ISBEhk9eGRmf_KfOfVo", "")
+BOT_TOKEN: str = os.environ.get("8869833081:AAFpbXQOmK7NtcPTHPj62thVh8TOOavbU8U", "")
 ADMIN_ID: int = 8796417770
 
 DEFAULT_CHANNEL_INVITE: str = "https://t.me/+mNXgNZ86X9RiNjg0"
@@ -12,9 +12,9 @@ DEFAULT_SUPPORT_USERNAME: str = "@TAMER_VIP"
 DEFAULT_CHANNEL_ID: str = ""
 DEFAULT_GUARANTEE_LINK: str = "https://t.me/+ZopATTV90psyODU0"
 
-FLY_IMAGE: Path = _BASE_DIR / "data" / "fly.jpg"
-APPLE_IMAGE: Path = _BASE_DIR / "data" / "apple.jpg"
-PLANE_IMAGE: Path = _BASE_DIR / "data" / "plane.jpg"
+FLY_IMAGE: Path = _BASE_DIR / "fly.jpg"
+APPLE_IMAGE: Path = _BASE_DIR / "apple.jpg"
+PLANE_IMAGE: Path = _BASE_DIR / "plane.jpg"
 
 APPS: dict[str, dict] = {
     "coldbet": {"name": "COLDBET", "emoji": "🔵", "promo": "FLY777"},
